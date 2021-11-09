74abbb1548a6beeab52b411a7b98779e32851b7d
Lukas Braunstorfer <braunstorfer.lukas@gmail.de>
2021-11-09 10:50:20 +0100

    remove unwanted first server start

--- start_script.sh
+++ start_script.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-
-cd /home/ubuntu/dps_bookstore
-
-python3 web_app/run.py
-
 while : ; do
     echo "start flask server"
     cd /home/ubuntu/dps_bookstore/ && python3 -u web_app/run.py >> web_app/run_log.log 2>&1
